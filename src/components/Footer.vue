b335a9c896d6fddae8fbc861e325c537af08ba04
Patr1ck Shaw <133486225+Patr1ck-74@users.noreply.github.com>
2025-09-05 11:16:30 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -59,7 +59,7 @@ const siteIcp = ref(import.meta.env.VITE_SITE_ICP);
 const siteAuthor = ref(import.meta.env.VITE_SITE_AUTHOR);
 const siteUrl = computed(() => {
   const url = import.meta.env.VITE_SITE_URL;
-  if (!url) return "https://patr1ck.qzz.io";
+  if (!url) return "https://patr1ck.cloudns.org";
   // 判断协议前缀
   if (!url.startsWith("http://") && !url.startsWith("https://")) {
     return "//" + url;
