02f421bd37cd845a6318854b6800fb86a322062f
Patr1ck Shaw <133486225+Patr1ck-74@users.noreply.github.com>
2025-09-05 16:55:09 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,20 +5,17 @@
         <span>
           <span :class="startYear < fullYear ? 'c-hidden' : 'hidden'">Copyright&nbsp;</span>
           &copy;
-          <span v-if="startYear < fullYear"
-            class="site-start">
-            {{ startYear }}
-            -
+          <span v-if="startYear < fullYear" class="site-start">
+            {{ startYear }} -
           </span>
           {{ fullYear }}
+          <!-- 自定义显示文字 -->
           <a :href="siteUrl">{{ siteAuthor }}</a>
         </span>
-        <!-- 以下信息请不要修改哦 -->
+        <!-- 以下信息请不要修改 -->
         <span class="hidden">
           &amp;&nbsp;Made&nbsp;by
-          <a :href="config.github" target="_blank">
-            {{ config.author }}
-          </a>
+          <a :href="config.github" target="_blank">{{ config.author }}</a>
         </span>
         <!-- 站点备案 -->
         <span>
@@ -42,30 +39,19 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { MusicOne } from "@icon-park/vue-next";
 import { mainStore } from "@/store";
 import config from "@/../package.json";
 
 const store = mainStore();
 const fullYear = new Date().getFullYear();
-
-// 加载配置数据
-// const siteStartDate = ref(import.meta.env.VITE_SITE_START);
-const startYear = ref(
-  import.meta.env.VITE_SITE_START?.length >= 4 ? 
-  import.meta.env.VITE_SITE_START.substring(0, 4) : null
-);
+const startYear = ref(import.meta.env.VITE_SITE_START?.substring(0, 4) || null);
 const siteIcp = ref(import.meta.env.VITE_SITE_ICP);
-const siteAuthor = ref(import.meta.env.VITE_SITE_AUTHOR);
-const siteUrl = computed(() => {
-  const url = import.meta.env.VITE_SITE_URL;
-  if (!url) return "https://www.imsyy.top";
-  // 判断协议前缀
-  if (!url.startsWith("http://") && !url.startsWith("https://")) {
-    return "//" + url;
-  }
-  return url;
-});
+
+// 自定义显示的文字
+const siteAuthor = ref("Patrick Shaw"); // 时钟旁边显示的文字
+const siteUrl = ref("#");               // 可以自定义链接，如果不需要可用 "#"
 </script>
 
 <style lang="scss" scoped>
@@ -79,12 +65,13 @@ const siteUrl = computed(() => {
   text-align: center;
   z-index: 0;
   font-size: 14px;
-  // 文字不换行
   word-break: keep-all;
   white-space: nowrap;
+
   .power {
     animation: fade 0.3s;
   }
+
   .lrc {
     padding: 0 20px;
     display: flex;
@@ -107,26 +94,31 @@ const siteUrl = computed(() => {
       }
     }
   }
+
   &.blur {
     backdrop-filter: blur(10px);
     background: rgb(0 0 0 / 25%);
     font-size: 16px;
   }
+
   .fade-enter-active,
   .fade-leave-active {
     transition: opacity 0.15s ease-in-out;
   }
+
   @media (max-width: 720px) {
     font-size: 0.9rem;
     &.blur {
       font-size: 0.9rem;
     }
   }
+
   @media (max-width: 560px) {
     .c-hidden {
       display: none;
     }
   }
+
   @media (max-width: 480px) {
     .hidden {
       display: none;
