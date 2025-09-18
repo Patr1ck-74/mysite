1c83068644498735eb5054eec92123d5f1d7d680
Patr1ck-74 <leslie.en.x@gmail.com>
2025-09-18 17:27:02 +0800

    新增图床链接：2025-09-18 17:27:02

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link,Tv, File, Bell, CompactDisc, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Link,Tv, File, Bell, CompactDisc, Images, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -67,7 +67,7 @@ const siteIcon = {
   Bell,
   File,
   CompactDisc,
-  Book,
+  Images,
   Fire,
   LaptopCode,
 };
